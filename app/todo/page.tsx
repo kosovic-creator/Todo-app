ceb0b6f80788a4ff483307681e8d1c620be991a6
kosovic-creator <drasko.kosovic@icloud.com>
2025-05-05 17:15:49 +0200

    init

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -115,11 +115,7 @@ export default function TodoTable() {
                         <button className="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 transition">Pregled</button>
                       </Link>
                     </div>
-                    {/* <div className="flex gap-2 flex-row-reverse w-full">
-                      <Link href={`/todo/${todo.id}`} >
-                        <button className="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 transition">Pregled</button>
-                      </Link>
-                    </div> */}
+           
                   </td>
                 </tr>
               ))
